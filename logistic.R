00fe9bc19e064eb34bb4103b1a0d37ee4c581067
PanosTsole87 <tsole87@yahoo.gr>
2018-11-23 19:19:16 +0000

    changes to models. Also added stepwise regression

--- logistic.R
+++ logistic.R
@@ -1,26 +1,29 @@
-# install.packages("rcompanion") # To observe the levels of the dummy variables
+install.packages("rcompanion") # To observe the levels of the dummy variables
 library(rcompanion)
 
-# install.packages("ROSE") # To contact ROC analysis and model comparison
+install.packages("ROSE") # To contact ROC analysis and model comparison
 library(ROSE)
 
 # install.packages("pscl") # To find R_sq
 library(pscl)
 
+install.packages("remotes")
 library(remotes) # To install a depracated package from archive
-# install_version("epicalc", "2.15.1.0") # To contact a LR test
+install_version("epicalc", "2.15.1.0") # To contact a LR test
 library(epicalc)
 
-# install.packages("extRemes") # To contact a LR test
+install.packages("extRemes") # To contact a LR test
 library(extRemes)
 
 # install.packages('caTools') # To split the dataset to train and test set
 library(caTools)
 
-# install.packages("corrplot") # To create correlation plots
+install.packages("corrplot") # To create correlation plots
 library(corrplot)
 
+install.packages("corrgram")
 library(corrgram)
+install.packages("Hmisc")
 library(Hmisc)
 
 # Load data_challenge_Script.R
@@ -95,7 +98,7 @@ model_d$gumd_binary = as.factor(model_d$gumd_binary)
 contrasts(model_d$gumd_binary)
 
 
-#### Correlations in clean_d ##############################################################################################
+#### Correlations in model_d ##############################################################################################
 str(model_d)
 summary(model_d)
 x = rcorr(as.matrix(model_d))
@@ -103,7 +106,7 @@ x = rcorr(as.matrix(model_d))
 corrplot(x$r)
 
 # Different corrplot 
-corrgram(model_d, order = F, upper.panel = panel.cor, main="gumd_binary")
+corrgram(model_d, order = F, upper.panel = panel.cor, main="gumd_binary", cor.method = "spearman")
 # Probing_depth is correlated with age, n_fillings. We need to remove it.
 # Also age is correlated with n_fillings. Maybe we need to remove the latter.
 
@@ -183,14 +186,14 @@ roc.curve(test_set$gumd_binary, prob_pred_model1) # AUC = 0.897
 logLik(model_1) # -264.83 df=11 (11 parameters)
 
 
-#### MODEL 2: Same as MODEL 1 BUT WITHOUT WATER_FLUOR #########################################################################
-# water_fluor is not statistically significant at the 95%, so we remove it
 
+
+#### MODEL 2: Same as MODEL 1 BUT WITHOUT SEX #########################################################################
 
 # Fitting Logistic Regression to the Training set 
-model_2 = glm(formula = gumd_binary ~ age + n.fillings + BMI + Flmins + alcohol + mouthwash + sex + smoke + typ2_diabetes,
-                 family = binomial,
-                 data = training_set)
+model_2 = glm(formula = gumd_binary ~ age + n.fillings + BMI + Flmins + alcohol + mouthwash + smoke + typ2_diabetes + water_fluor,
+              family = binomial,
+              data = training_set[-7])
 
 # See outputs
 summary(model_2)
@@ -199,86 +202,53 @@ sink("model_2.txt")
 print(summary(model_2))
 sink() 
 
-
+# All variables are statistically significant, except water_fluor (significant at the 90% level)
 
 #### Find R_sq ################################################################
 
-round(pR2(model_2)[4], 3) # McFadden R_sq = 0.476
+round(pR2(model_2)[4], 3) # McFadden R_sq = 0.382
 
 #### Predicting the Test set results #####################################################
-prob_pred_model2 = predict(model_2, type = "response", newdata = test_set[-11])
+prob_pred_model2 = predict(model_2, type = "response", newdata = test_set[-c(7,11)])
 y_pred = ifelse(prob_pred_model2 > 0.5, 1, 0)
 
 # Making the Confusion matrix
 cm = table(test_set[, 11], y_pred > 0.5) 
 # True Negatives (TN) = 150
-# True Positives (TP) = 55
-# False Negatives (FN) = 21
+# True Positives (TP) = 51
+# False Negatives (FN) = 25
 # False Positives (FP) = 20
 # Accuracy = (TP + TN) / (TN + TP + FN + FP) - Accurate Positive and Negative Predictions
-Accuracy_model2 = round((cm[1, 1] + cm[2, 2]) / (cm[1, 1] + cm[2, 2] + cm[1, 2] + cm[2, 1]), 2) # 0.83
+Accuracy_model2 = round((cm[1, 1] + cm[2, 2]) / (cm[1, 1] + cm[2, 2] + cm[1, 2] + cm[2, 1]), 2) # 0.82
 # Precision = TP / Total Predicted Positive (FP + TP) - How precise are the positive predictions 
-Precision_model2 = round(cm[2, 2] / (cm[1, 2] + cm[2, 2]), 2) # 0.73
+Precision_model2 = round(cm[2, 2] / (cm[1, 2] + cm[2, 2]), 2) # 0.72
 # Recall = TP / Total Actual Positive (TP + FN) - How many of the Actual Positives are predicted accurately as Positives
-Recall_model2 = round(cm[2, 2] / (cm[2, 1] + cm[2, 2]), 2) # 0.72
+Recall_model2 = round(cm[2, 2] / (cm[2, 1] + cm[2, 2]), 2) # 0.67
 # Recall is extremely important in disease detection. If a sick patient (actual positive)
 # is diagnosed as non patient (False Negative), it can have significant impacts to his/her life.
 
 # F1 score = 2 * Precision * Recall / (Precision + Recall) - It is the harmonic mean of Precision and Recall
-F1score_model2 = round(2*Precision_model2*Recall_model2 / (Precision_model2 + Recall_model2), 2) # 0.72
+F1score_model2 = round(2*Precision_model2*Recall_model2 / (Precision_model2 + Recall_model2), 2) # 0.69
 
 # ROC analysis
-roc.curve(test_set$gumd_binary, prob_pred_model2) # 0.898
+roc.curve(test_set$gumd_binary, prob_pred_model2) # 0.901
 
 # Likelihood Ratio Test
-logLik(model_2) # -266.53
-lr.test(logLik(model_2), logLik(model_1)) # -2(LL2 - LL1)
-
-# I can't find another variable to remove at this point. Sex is statistically significant at the 99%.
+logLik(model_3) # -282.77
+lr.test(logLik(model_3), logLik(model_1)) # -2(LL2 - LL1)
 
 
-#### FORWARD SELECTION ##################################################################################
-#### MODEL 1: Find the variable with the lowest p-value #########################################################################
 
-# Fitting Logistic Regression to the Training set 
-model_11 = glm(formula = gumd_binary ~ age,
-              family = binomial,
-              data = training_set)
+#### Stepwise regression ######################################################
 
-# See outputs
-summary(model_11)
+# install.packages("leaps")
+library(leaps)
+library('car')
 
-# Fitting Logistic Regression to the Training set 
-model_12 = glm(formula = gumd_binary ~ n.fillings,
-               family = binomial,
-               data = training_set)
+stepwise_reg = regsubsets(gumd_binary ~ age + n.fillings + BMI + Flmins + alcohol + mouthwash + smoke + typ2_diabetes + water_fluor, data = model_d, nbest = 1) # Find only the best of each variable-number category
 
-# See outputs
-summary(model_12) # n.fillings more significant than age
+summary(stepwise_reg)
 
-# Fitting Logistic Regression to the Training set 
-model_13 = glm(formula = gumd_binary ~ BMI,
-               family = binomial,
-               data = training_set)
+subsets(stepwise_reg, statistic = 'rsq')
 
-# See outputs
-summary(model_13) # n.fillings is more significant than BMI
-
-# Fitting Logistic Regression to the Training set 
-model_14 = glm(formula = gumd_binary ~ Flmins,
-               family = binomial,
-               data = training_set)
-
-# See outputs
-summary(model_14) # n.fillings is more significant than Flmins
-
-
-# Fitting Logistic Regression to the Training set 
-model_15 = glm(formula = gumd_binary ~ Flmins,
-               family = binomial,
-               data = training_set)
-
-# See outputs
-summary(model_14) # n.fillings is more significant than Flmins
-
-# NOT FINISHED. JUST GOT BORED!!!
+# Stepwise regression leads to the same final output - All variables are statistically significant except water_fluor
